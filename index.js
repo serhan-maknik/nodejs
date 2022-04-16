f997b9384f16956beef83bea91e637840a2e48fe
serhan-maknik <maqnik@gmail.com>
2022-04-16 10:06:48 +0300

    knex

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const aktorlerRouter = require('./routers/aktorlerRouter')
 let data = require('./data')
 
 const logger = require('./middlewares/logger')
+const errorHandling = require('./middlewares/errorHandling')
 
 const server = express();
 
@@ -11,23 +12,35 @@ server.use(express.json())
 
 server.use(logger)
 
+
 server.use('/aktorler', aktorlerRouter)
 
 server.get('/', (req, res) => {
     res.send('Express is görüyor')
 })
 
-
-
 let count = 4
-server.post("/post", (req, res) => {
+server.post("/post", (req, res, next) => {
     let datas = req.body
-    datas.id = count;
-    data.push(datas);
-    count++;
-    console.log("cevap: ", data)
-    res.send(data);
+    if (datas.isim) {
+        datas.id = count;
+        data.push(datas);
+        count++;
+        console.log("cevap: ", data)
+        res.send(data);
+    } else {
+        next({
+            statusCode: 400,
+            errorMessage: "Aktor eklemek için isim girmelisiniz",
+        })
+    }
+
+
 })
+
+
+//bunu her zaman en altta listenin üstünde tutuyoruz 
+server.use(errorHandling)
 
 server.listen(5000, () => {
 
--- middlewares/errorHandling.js
+++ middlewares/errorHandling.js
@@ -1,3 +1,4 @@
-module.exports = () => {
-
+module.exports = (err, req, res, next) => {
+    console.log("ERORRORRR: ", err);
+    res.status(err.statusCode).json(err)
 }
